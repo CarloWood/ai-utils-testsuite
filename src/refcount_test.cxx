672dc73c23ec716fc8f80ba482b8efe32dc8dbf3
Carlo Wood <carlo@alinoe.com>
2017-01-29 02:23:27 +0100

    Compile fix for ref_count() name change.

--- src/refcount_test.cxx
+++ src/refcount_test.cxx
@@ -14,51 +14,51 @@ struct Test : public AIRefCount {
 int main()
 {
   boost::intrusive_ptr<Test> p1 = new Test;
-  assert(p1->unique() && p1->count() == 1);
+  assert(p1->unique() && p1->ref_count() == 1);
 
   boost::intrusive_ptr<Test> p2 = p1;
   {
     boost::intrusive_ptr<Test> p3(p1);
-    assert(!p1->unique() && p3->count() == 3);
+    assert(!p1->unique() && p3->ref_count() == 3);
     p2.reset();
-    assert(!p1->unique() && p3->count() == 2);
+    assert(!p1->unique() && p3->ref_count() == 2);
   }
-  assert(p1->unique() && p1->count() == 1);
+  assert(p1->unique() && p1->ref_count() == 1);
 
   Test* p = p1.get();
   p2 = p;
   boost::intrusive_ptr<Test> p3 = p;
-  assert(p1->count() == 3);
-  assert(p2->count() == 3);
-  assert(p3->count() == 3);
+  assert(p1->ref_count() == 3);
+  assert(p2->ref_count() == 3);
+  assert(p3->ref_count() == 3);
 
   boost::intrusive_ptr<Test> q1 = new Test;
   boost::intrusive_ptr<Test> q2(q1);
-  assert(q1->count() == 2);
-  assert(q2->count() == 2);
+  assert(q1->ref_count() == 2);
+  assert(q2->ref_count() == 2);
 
   std::swap(p2, q2);
-  assert(p1->count() == 3);
-  assert(p2->count() == 2);     // swapped with q2
-  assert(p3->count() == 3);
-  assert(q1->count() == 2);
-  assert(q2->count() == 3);     // swapped with p2
+  assert(p1->ref_count() == 3);
+  assert(p2->ref_count() == 2);     // swapped with q2
+  assert(p3->ref_count() == 3);
+  assert(q1->ref_count() == 2);
+  assert(q2->ref_count() == 3);     // swapped with p2
 
   boost::intrusive_ptr<Test> r1 = new Test(*p);
   assert(r1->unique());
   // No change:
-  assert(p1->count() == 3);
-  assert(p2->count() == 2);
-  assert(p3->count() == 3);
-  assert(q1->count() == 2);
-  assert(q2->count() == 3);
+  assert(p1->ref_count() == 3);
+  assert(p2->ref_count() == 2);
+  assert(p3->ref_count() == 3);
+  assert(q1->ref_count() == 2);
+  assert(q2->ref_count() == 3);
 
   p2 = q2;
-  assert(p1->count() == 4);
-  assert(p2->count() == 4);     // same as q2
-  assert(p3->count() == 4);
-  assert(q1->count() == 1);     // decremented with 1.
-  assert(q2->count() == 4);     // incremented with 1.
+  assert(p1->ref_count() == 4);
+  assert(p2->ref_count() == 4);     // same as q2
+  assert(p3->ref_count() == 4);
+  assert(q1->ref_count() == 1);     // decremented with 1.
+  assert(q2->ref_count() == 4);     // incremented with 1.
 
   std::cout << p1->test() << std::endl;
 }

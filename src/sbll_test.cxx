dd34ab86bc55d2f055d68552338f1a41beb9e257
Carlo Wood <carlo@alinoe.com>
2018-06-14 17:45:59 +0200

    WIP

--- src/sbll_test.cxx
+++ src/sbll_test.cxx
@@ -4,7 +4,9 @@
 struct XxxxData
 {
   int n;
+
   XxxxData(int n_) : n(n_) { }
+
   friend bool operator==(XxxxData const& d1, XxxxData const& d2) { return d1.n == d2.n; }
   friend bool operator<(XxxxData const& d1, XxxxData const& d2) { return d1.n < d2.n; }
 };
@@ -17,30 +19,37 @@ class XxxxNode : public utils::SbllNode<utils::SbllNodeImpl, XxxxNode>
   XxxxNode(int data) : m_data(data) { }
 
  protected:
-  utils::lteqgt_nt insert_cmp(XxxxNode const& Xxxx_data) const override
+  lteqgt_nt insert_cmp(XxxxNode const& Xxxx_data) const override
   {
-    return Xxxx_data.m_data == m_data ? utils::EQUAL : Xxxx_data.m_data < m_data ? utils::LESS : utils::GREATER;
+    return Xxxx_data.m_data == m_data ? EQUAL : Xxxx_data.m_data < m_data ? LESS : GREATER;
   }
 
   bool find_cmp(void const* key) const override
   {
-    XxxxNode const* ptr = static_cast<XxxxNode const*>(key);
-    return m_data == ptr->m_data;
+    XxxxData const* data = static_cast<XxxxData const*>(key);
+    return m_data == *data;
   }
 
   void delinked(void) override
   {
+    std::cout << "Calling delinked() for node '" << m_data.n << "'" << std::endl;
   }
 
   void done(void) override
   {
+    std::cout << "Calling done() for node '" << m_data.n << "'" << std::endl;
+    if (3 <= m_data.n && m_data.n <= 5)
+    {
+      prev_node()->del();
+      next_node()->del();
+    }
   }
 
   void deceased(utils::SbllNodeImpl* UNUSED_ARG(node), void* UNUSED_ARG(label)) override
   {
   }
 
-  DEBUGLLISTS_LLISTS_METHODS(XxxxNode);
+  LLISTS_METHODS(XxxxNode);
 };
 
 using XxxxList = utils::SbllList<utils::SbllListImpl, XxxxNode>;
@@ -50,11 +59,16 @@ int main()
   Debug(NAMESPACE_DEBUG::init());
 
   XxxxList foo("Xxxx");
-  XxxxNode* Xxxx_node;
-  XxxxNode* bar;
 
-  bar = new XxxxNode(42);
-  foo.add(bar);
-  Xxxx_node = foo.find(bar);
-  Xxxx_node->del();
+  for (int i = 0; i < 10; ++i)
+    foo.add(new XxxxNode(i));
+
+  XxxxData key(4);
+  XxxxNode* Xxxx_node = foo.find(&key);
+  if (Xxxx_node)
+    Xxxx_node->del();
+  else
+    std::cout << "Could not find 4." << std::endl;
+
+  Dout(dc::notice|flush_cf, "Leaving main()");
 }

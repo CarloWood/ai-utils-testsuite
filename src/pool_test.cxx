e9b5d6088ad47ec27827e28a1fe97d5cdaf232df
Carlo Wood <carlo@alinoe.com>
2018-09-18 00:45:15 +0200

    Updated utils::NodeMemoryPool.

--- src/pool_test.cxx
+++ src/pool_test.cxx
@@ -3,6 +3,7 @@
 #include "utils/NodeMemoryPool.h"
 #include <vector>
 #include <algorithm>
+#include <cstring>
 
 unsigned int seed = 4321;
 
@@ -10,10 +11,9 @@ struct Foo {
   char a[120];
 };
 
-int main()
+void test1()
 {
-  Debug(NAMESPACE_DEBUG::init());
-
+  DoutEntering(dc::notice, "test1()");
   utils::NodeMemoryPool pool(128);
 
   size_t num_mallocs = 0;
@@ -46,5 +46,37 @@ int main()
 
   Dout(dc::notice, "Did " << num_mallocs << " allocations and frees.");
   Dout(dc::notice, pool);
+}
+
+struct Bar {
+  int n;
+  char a[30 - sizeof(int)];
+
+  Bar(int n_) : n(n_) { }
+  ~Bar() { std::memset(this, 0, 30); }
+
+  void operator delete(void* ptr) { utils::NodeMemoryPool::static_free(ptr); }
+};
+
+struct ExtendedBar : public Bar {
+  unsigned short m_x;
+};
+
+void test2()
+{
+  DoutEntering(dc::notice, "test2()");
+  utils::NodeMemoryPool pool(32, sizeof(Bar) + 8);          // Allocate chunks that are larger than the object.
+  Bar* bar = new(pool) Bar(42);
+  ASSERT(bar->n == 42);
+  reinterpret_cast<ExtendedBar*>(bar)->m_x = 0x1234;        // Secret space after the object ;).
+  delete bar;
+  ASSERT(reinterpret_cast<ExtendedBar*>(bar)->m_x == 0x1234);
+}
+
+int main()
+{
+  Debug(NAMESPACE_DEBUG::init());
+  test1();
+  test2();
   Dout(dc::notice, "Leaving main()...");
 }

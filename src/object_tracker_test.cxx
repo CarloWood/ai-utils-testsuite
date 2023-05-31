e35e19bc0508ad3b9c7b25e09452e0f34cc1b9bc
Carlo Wood <carlo@alinoe.com>
2023-05-31 17:41:48 +0200

    Add object_tracker_test.cxx

---
+++ src/object_tracker_test.cxx
@@ -0,0 +1,36 @@
+#include "sys.h"
+#include "utils/ObjectTracker.h"
+#include "debug.h"
+
+struct Node;
+
+class NodeTracker : public utils::ObjectTracker<Node>
+{
+ public:
+  NodeTracker(Node* tracked) : utils::ObjectTracker<Node>(tracked) { }
+};
+
+class Node : public utils::TrackedObject<NodeTracker>
+{
+ private:
+  std::string s_;
+
+ public:
+  Node(std::string const& s) : s_(s) { }
+
+  std::string const& s() const { return s_; }
+};
+
+int main()
+{
+  Debug(NAMESPACE_DEBUG::init());
+  Dout(dc::notice, "Entered main()...");
+
+  Node node("This is a long string so that it won't be put inside the std::string.");
+  std::weak_ptr<NodeTracker> node_tracker = node;
+  Node node2(std::move(node));
+  ASSERT(node.s().empty());
+  std::cout << "tracked.s = " << node_tracker.lock()->tracked_object().s() << std::endl;
+
+  Dout(dc::notice, "Leaving main()...");
+}
